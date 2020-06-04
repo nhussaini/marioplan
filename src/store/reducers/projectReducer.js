33d3146033bca729face08ae3b275bbe51c20902
unknown <nhussaini@luc.edu>
2020-06-03 20:12:25 -0700

    added dummy data in redux and displayed in the comps

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {}
+const initState = {
+    projects : [
+        {id: '1', title: 'help me find pitch', content: 'bla bla bla'},
+        {id: '2', title: 'collect all the stars', content: 'bla bla bla'},
+        {id: '3', title: 'egg hunt with Yoshi', content: 'bla bla bla'}
+    ]
+}
 
 const projectReducer = (state = initState, action)=>{
     return state
