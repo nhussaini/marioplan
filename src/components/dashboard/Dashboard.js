24771f901a8f229140df8b34b167746977d780c5
unknown <nhussaini@luc.edu>
2020-06-05 16:27:21 -0700

    fetched project detail data from firestore

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -7,7 +7,7 @@ import { compose } from 'redux';
 
 class Dashboard extends Component {
     render(){
-        console.log(this.props);
+       // console.log(this.props);
         const { projects } = this.props
         return (
             <div className="dashboard container">
