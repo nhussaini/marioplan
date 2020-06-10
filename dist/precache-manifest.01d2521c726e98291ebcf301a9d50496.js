21adcad4cad9e2cddb7036eee1469e3e0a0cf7f1
unknown <nhussaini@luc.edu>
2020-06-09 21:12:40 -0700

    deployed on the firebase host

---
+++ dist/precache-manifest.01d2521c726e98291ebcf301a9d50496.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ec87f133745de69d965c2f4f017a5cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2283236e8ffbc2ec7980",
+    "url": "/static/css/main.09692269.chunk.css"
+  },
+  {
+    "revision": "1ab9477f11f1877b1d1b",
+    "url": "/static/js/2.9002bdd1.chunk.js"
+  },
+  {
+    "revision": "f42d151f61898dd9d5ffe0d9c5238b3c",
+    "url": "/static/js/2.9002bdd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2283236e8ffbc2ec7980",
+    "url": "/static/js/main.7592b9c3.chunk.js"
+  },
+  {
+    "revision": "98a629c2f8b7a0ef59a4",
+    "url": "/static/js/runtime-main.0d165b64.js"
+  }
+]);
